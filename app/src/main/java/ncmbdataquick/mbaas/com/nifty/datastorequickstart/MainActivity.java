5151d6cb74091968aa0f4457d96a0082b66faf4d
Apple <apple@Apple-no-MacBook-Air.local>
2016-04-14 14:29:55 +0900

    first version

---
+++ app/src/main/java/ncmbdataquick/mbaas/com/nifty/datastorequickstart/MainActivity.java
@@ -0,0 +1,78 @@
+package ncmbdataquick.mbaas.com.nifty.datastorequickstart;
+
+import android.app.AlertDialog;
+import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
+import android.view.Menu;
+import android.view.MenuItem;
+
+import com.nifty.cloud.mb.core.DoneCallback;
+import com.nifty.cloud.mb.core.NCMB;
+import com.nifty.cloud.mb.core.NCMBException;
+import com.nifty.cloud.mb.core.NCMBObject;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+        NCMB.initialize(this, "APP_KEY", "CLIENT_KEY");
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+
+    public void doStartDemo(View view) {
+        final NCMBObject obj = new NCMBObject("TestClass");
+        obj.put("message", "Hello, NCMB!");
+        obj.saveInBackground(new DoneCallback() {
+            @Override
+            public void done(NCMBException e) {
+                if (e != null) {
+                    //保存失敗
+                    new AlertDialog.Builder(MainActivity.this)
+                            .setTitle("Notification from Nifty")
+                            .setMessage("Error:" + e.getMessage())
+                            .setPositiveButton("OK", null)
+                            .show();
+
+                } else {
+                    //保存成功
+                    new AlertDialog.Builder(MainActivity.this)
+                            .setTitle("Notification from Nifty")
+                            .setMessage("Save successfull! with ID:" + obj.getObjectId())
+                            .setPositiveButton("OK", null)
+                            .show();
+
+                }
+            }
+        });
+
+    }
+}
