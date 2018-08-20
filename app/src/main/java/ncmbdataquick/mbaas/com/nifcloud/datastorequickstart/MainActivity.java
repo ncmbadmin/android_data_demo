4b1894e0cbc9263e6d9915b3102cf58a272e10b9
TamHH <vfa.tamhh@gmail.com>
2018-08-20 11:53:07 +0700

    change_Nifty_text final

--- app/src/main/java/ncmbdataquick/mbaas/com/nifcloud/datastorequickstart/MainActivity.java
+++ app/src/main/java/ncmbdataquick/mbaas/com/nifcloud/datastorequickstart/MainActivity.java
@@ -1,19 +1,18 @@
-package ncmbdataquick.mbaas.com.nifty.datastorequickstart;
+package ncmbdataquick.mbaas.com.nifcloud.datastorequickstart;
 
 import android.app.AlertDialog;
 import android.os.Bundle;
-import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
 
-import com.nifty.cloud.mb.core.DoneCallback;
-import com.nifty.cloud.mb.core.NCMB;
-import com.nifty.cloud.mb.core.NCMBException;
-import com.nifty.cloud.mb.core.NCMBObject;
+import com.nifcloud.mbaas.core.DoneCallback;
+import com.nifcloud.mbaas.core.NCMB;
+import com.nifcloud.mbaas.core.NCMBException;
+import com.nifcloud.mbaas.core.NCMBObject;
+
 
 public class MainActivity extends AppCompatActivity {
 
@@ -23,7 +22,7 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
-        
+
         //**************** APIキーの設定とSDKの初期化 **********************
         NCMB.initialize(this.getApplicationContext(), "YOUR_APPLICATION_KEY", "YOUR_CLIENT_KEY");
     }
@@ -52,29 +51,34 @@ public class MainActivity extends AppCompatActivity {
 
     public void doStartDemo(View view) {
         final NCMBObject obj = new NCMBObject("TestClass");
-        obj.put("message", "Hello, NCMB!");
-        obj.saveInBackground(new DoneCallback() {
-            @Override
-            public void done(NCMBException e) {
-                if (e != null) {
-                    //保存失敗
-                    new AlertDialog.Builder(MainActivity.this)
-                            .setTitle("Notification from Nifty")
-                            .setMessage("Error:" + e.getMessage())
-                            .setPositiveButton("OK", null)
-                            .show();
+        try {
+            obj.put("message", "Hello, NCMB!");
+            obj.saveInBackground(new DoneCallback() {
+                @Override
+                public void done(NCMBException e) {
+                    if (e != null) {
+                        //保存失敗
+                        new AlertDialog.Builder(MainActivity.this)
+                                .setTitle("Notification from NIFCLOUD")
+                                .setMessage("Error:" + e.getMessage())
+                                .setPositiveButton("OK", null)
+                                .show();
 
-                } else {
-                    //保存成功
-                    new AlertDialog.Builder(MainActivity.this)
-                            .setTitle("Notification from Nifty")
-                            .setMessage("Save successfull! with ID:" + obj.getObjectId())
-                            .setPositiveButton("OK", null)
-                            .show();
+                    } else {
+                        //保存成功
+                        new AlertDialog.Builder(MainActivity.this)
+                                .setTitle("Notification from NIFCLOUD")
+                                .setMessage("Save successfull! with ID:" + obj.getObjectId())
+                                .setPositiveButton("OK", null)
+                                .show();
 
+                    }
                 }
-            }
-        });
+            });
+        } catch (NCMBException e) {
+            e.printStackTrace();
+        }
+
 
     }
 }
