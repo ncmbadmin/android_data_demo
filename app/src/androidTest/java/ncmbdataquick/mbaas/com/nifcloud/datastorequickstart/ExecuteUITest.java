e15e36bbe1077af14a43a60e2fe1d77c7ceba0f4
vfa-tamhh <vfa.tamhh@gmail.com>
2021-02-22 14:31:35 +0700

    add annotation

--- app/src/androidTest/java/ncmbdataquick/mbaas/com/nifcloud/datastorequickstart/ExecuteUITest.java
+++ app/src/androidTest/java/ncmbdataquick/mbaas/com/nifcloud/datastorequickstart/ExecuteUITest.java
@@ -3,6 +3,7 @@ package ncmbdataquick.mbaas.com.nifcloud.datastorequickstart;
 import androidx.test.espresso.ViewInteraction;
 import androidx.test.ext.junit.rules.ActivityScenarioRule;
 import androidx.test.ext.junit.runners.AndroidJUnit4;
+import androidx.test.filters.LargeTest;
 
 import org.junit.Before;
 import org.junit.Rule;
@@ -23,6 +24,7 @@ import static org.hamcrest.core.StringContains.containsString;
  * @see <a href="http://d.android.com/tools/testing">Testing documentation</a>
  */
 @RunWith(AndroidJUnit4.class)
+@LargeTest
 public class ExecuteUITest {
 
     ViewInteraction btnStart;
